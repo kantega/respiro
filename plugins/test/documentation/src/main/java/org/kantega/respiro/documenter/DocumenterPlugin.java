99a2655ba408eb59f91180da1e83d0d1dcb583f3
Atle Prange <atle.prange@gmail.com>
2016-06-14 15:50:55 +0200

    Single json for doc

--- plugins/test/documentation/src/main/java/org/kantega/respiro/documenter/DocumenterPlugin.java
+++ plugins/test/documentation/src/main/java/org/kantega/respiro/documenter/DocumenterPlugin.java
@@ -157,14 +157,14 @@ public class DocumenterPlugin {
             logger.info("**** Plugin docs ****");
             logger.info(desc);
 
-            PluginDocumentation doc = new PluginDocumentation(desc, dependencies.toList(), routeDocumentations, resourceDocs);
+            Model model = ModelBuilder.extractModel(routeDocumentations);
+            Map<String,Object> json = ModelBuilder.toJson(model );
+            PluginDocumentation doc = new PluginDocumentation(desc, dependencies.toList(), routeDocumentations, resourceDocs, json);
 
 
             mapper.writeValue(targetDirectory.resolve("doc.json").toFile(), doc);
 
-            Model model = ModelBuilder.extractModel(routeDocumentations);
-            Map<String,Object> json = ModelBuilder.toJson(model );
-            mapper.writeValue(targetDirectory.resolve("model.json").toFile(),json);
+
 
         }
         catch (Throwable e) {
--- plugins/test/documentation/src/main/java/org/kantega/respiro/documenter/PluginDocumentation.java
+++ plugins/test/documentation/src/main/java/org/kantega/respiro/documenter/PluginDocumentation.java
@@ -3,21 +3,25 @@ package org.kantega.respiro.documenter;
 import fj.data.List;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Map;
+
 public class PluginDocumentation {
 
     public final String pluginDescription;
     public final List<DependencyDocumentation> dependencies;
     public final List<RouteDocumentation> routes;
     public final List<ResourceDocumentation> resources;
+    public final Map<String, Object> model;
 
     public PluginDocumentation(
       String pluginDescription,
       List<DependencyDocumentation> dependencies,
       List<RouteDocumentation> routes,
-      List<ResourceDocumentation> resources) {
-        this.pluginDescription = StringUtils.replace(pluginDescription,"*","");
+      List<ResourceDocumentation> resources, Map<String, Object> model) {
+        this.pluginDescription = StringUtils.replace(pluginDescription, "*", "");
         this.dependencies = dependencies;
         this.routes = routes;
         this.resources = resources;
+        this.model = model;
     }
 }
